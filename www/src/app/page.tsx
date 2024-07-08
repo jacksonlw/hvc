92162da5080d7e78391736466fe9369d4c64bb7e
Jackson Lawrence <jlawrence1787@gmail.com>
2024-07-08 06:58:17 -0700

    removed excess padding on lading page

--- www/src/app/page.tsx
+++ www/src/app/page.tsx
@@ -15,44 +15,42 @@ export default async function HomePage() {
   const events = await listTenEvents(env.GOOGLE_EVENTS_CALENDAR_ID);
 
   return (
-    <>
-      <div className="pt-navOffset mb-16 grid grid-cols-2 gap-16">
-        <LandingHeader className="pb-navOffset min-h-[calc(100dvh-theme(spacing.navOffset))]" />
-        <div className="pb-navOffset flex items-center justify-end">
-          <Image
-            src={IMAGES.homeBackground}
-            width={1920}
-            height={1080}
-            alt="Portrait of Hayward, CA"
-            className="relative aspect-square rounded-full border-8 border-violet-200 object-cover object-top ring-8 ring-violet-500"
-          />
-        </div>
-        <AboutUsSection className="mb-32" />
-        <div className="">
-          <Image
-            src={IMAGES.members}
-            width={1920}
-            height={1080}
-            alt="Portrait of club members"
-            className="aspect-video rounded-3xl border-4 border-white object-cover object-top"
-          />
-        </div>
-        <EventsSection className="col-span-full mb-32" events={events} />
-        <ReservationsSection />
-        <div className="">
-          <Image
-            src={IMAGES.clubhouse}
-            width={1920}
-            height={1080}
-            alt="Portrait of club members"
-            className="aspect-video rounded-3xl border-4 border-white object-cover object-top"
-          />
-        </div>
-        <ContactUsSection className="min-h-dvh" />
-        <div className="flex">
-          <ContactForm className="h-fit w-full" />
-        </div>
+    <div className="mb-16 grid grid-cols-2 gap-16">
+      <LandingHeader className="min-h-[calc(100dvh-theme(spacing.navOffset))] pb-navOffset" />
+      <div className="flex items-center justify-end pb-navOffset">
+        <Image
+          src={IMAGES.homeBackground}
+          width={1920}
+          height={1080}
+          alt="Portrait of Hayward, CA"
+          className="relative aspect-square rounded-full border-8 border-violet-200 object-cover object-top ring-8 ring-violet-500"
+        />
       </div>
-    </>
+      <AboutUsSection className="mb-32" />
+      <div className="">
+        <Image
+          src={IMAGES.members}
+          width={1920}
+          height={1080}
+          alt="Portrait of club members"
+          className="aspect-video rounded-3xl border-4 border-white object-cover object-top"
+        />
+      </div>
+      <EventsSection className="col-span-full mb-32" events={events} />
+      <ReservationsSection />
+      <div className="">
+        <Image
+          src={IMAGES.clubhouse}
+          width={1920}
+          height={1080}
+          alt="Portrait of club members"
+          className="aspect-video rounded-3xl border-4 border-white object-cover object-top"
+        />
+      </div>
+      <ContactUsSection className="min-h-dvh" />
+      <div className="flex">
+        <ContactForm className="h-fit w-full" />
+      </div>
+    </div>
   );
 }
