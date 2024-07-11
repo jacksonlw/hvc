6d7161006e78d6c54685cc5cb7ac892274e374a8
Jackson Lawrence <jlawrence1787@gmail.com>
2024-07-10 20:39:15 -0700

    added ring to textfield

--- www/src/components/TextField.tsx
+++ www/src/components/TextField.tsx
@@ -14,7 +14,7 @@ export const TextField = (props: TextFieldProps) => {
     <Component
       type="text"
       className={twMerge(
-        "focus:bg-violet-25 w-full rounded-xl border border-gray-400 bg-gray-50 p-2.5 transition-[border-color,background-color] placeholder:text-gray-500 focus:border-violet-600 focus:outline-none focus:placeholder:text-gray-500",
+        "w-full rounded-xl border border-gray-400 bg-gray-50 p-2.5 ring-violet-300 transition-[border-color,background-color] placeholder:text-gray-500 focus:border-violet-600 focus:bg-violet-25 focus:outline-none focus:ring-2 focus:placeholder:text-gray-500",
         multiline && "h-32 resize-none",
         className,
       )}
