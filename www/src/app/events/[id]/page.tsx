a6eb9cf1c8a2eb4c593b0f91f25c700f0eace0d5
Jackson Lawrence <jlawrence1787@gmail.com>
2024-07-28 16:35:42 -0700

    made event view page responsive

--- www/src/app/events/[id]/page.tsx
+++ www/src/app/events/[id]/page.tsx
@@ -35,17 +35,17 @@ export default async function EventPage(props: EventPageProps) {
   );
 
   return (
-    <div className="grid min-h-[calc(100svh-theme(spacing.navOffset))] grid-cols-4 gap-6">
-      <div className="sticky top-navOffset self-start py-6">
+    <div className="flex min-h-[calc(100svh-theme(spacing.navOffset))] flex-col gap-6 xl:grid xl:grid-cols-4">
+      <div className="top-navOffset flex items-start gap-8 self-start pt-4 xl:sticky xl:flex-col xl:pb-4">
         <BackButton
           variant="outline"
-          className="-ml-4 mb-8 border-none text-black hover:text-violet-600"
+          className="-ml-4 hidden border-none !bg-transparent text-black transition-none hover:text-violet-600 active:text-violet-400 lg:flex"
         >
-          <ArrowLeftIcon className="mr-2 transition-[stroke]" />
+          <ArrowLeftIcon className="mr-2" />
           Go Back
         </BackButton>
 
-        <div className="grid gap-4">
+        <div className="grid grid-cols-2 gap-4 xl:grid-cols-1">
           <div>
             <p className="mb-1 text-sm text-gray-500">Event</p>
             <Heading className="text-xl">{event.name}</Heading>
@@ -66,15 +66,15 @@ export default async function EventPage(props: EventPageProps) {
           </div>
         </div>
       </div>
-      <div className="col-span-3 py-6">
+      <div className="flex grow flex-col pb-4 xl:col-span-3 xl:pt-4">
         {event.attachmentFileId ? (
           <EmbedPDF
-            className="h-full w-full"
+            className="grow"
             url={`https://drive.google.com/file/d/${event.attachmentFileId}/preview`}
           />
         ) : (
-          <div className="flex h-full w-full items-center justify-center rounded-xl border border-gray-300 bg-gray-100">
-            <p className="font-heading text-xl text-gray-600">
+          <div className="flex grow items-center justify-center rounded-xl border border-gray-300 bg-gray-100 p-4">
+            <p className="text-center font-heading text-gray-600 md:text-xl">
               No additional details are provided for this event
             </p>
           </div>
--- www/src/features/embed/EmbedPDF.tsx
+++ www/src/features/embed/EmbedPDF.tsx
@@ -1,7 +1,7 @@
 import { type HTMLAttributes } from "react";
 import { twMerge } from "tailwind-merge";
 
-type EmbedPDFProps = HTMLAttributes<HTMLDivElement> & {
+type EmbedPDFProps = HTMLAttributes<HTMLEmbedElement> & {
   url: string;
 };
 
@@ -9,11 +9,13 @@ export const EmbedPDF = (props: EmbedPDFProps) => {
   const { className, url, ...rest } = props;
 
   return (
-    <div className={twMerge("", className)} {...rest}>
-      <embed
-        src={url}
-        className="h-full w-full rounded-lg border-8 border-gray-300"
-      />
-    </div>
+    <embed
+      src={url}
+      className={twMerge(
+        "h-full w-full rounded-lg border-2 border-gray-300 lg:border-8",
+        className,
+      )}
+      {...rest}
+    />
   );
 };
