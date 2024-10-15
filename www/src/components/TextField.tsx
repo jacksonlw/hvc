8feb68982deee64973a8cf23a77774004e74bd70
Jackson Lawrence <jlawrence1787@gmail.com>
2024-10-14 20:36:48 -0400

    lighter placeholder text color

--- www/src/components/TextField.tsx
+++ www/src/components/TextField.tsx
@@ -18,7 +18,7 @@ export const TextField = forwardRef<
   const { className, multiline, ...rest } = props;
 
   const mergedClasses = twMerge(
-    "w-full rounded-xl border border-gray-400 bg-gray-50 p-2.5 ring-violet-300 transition-[border-color,background-color] placeholder:text-gray-600 hover:border-gray-600 focus:border-violet-600 focus:bg-violet-25 focus:outline-none focus:ring-2 focus:placeholder:text-gray-600",
+    "w-full rounded-xl border border-gray-400 bg-gray-50 p-2.5 ring-violet-300 transition-[border-color,background-color] placeholder:text-gray-500 hover:border-gray-600 focus:border-violet-600 focus:bg-violet-25 focus:outline-none focus:ring-2 focus:placeholder:text-gray-500",
     multiline && "h-32 resize-none",
     className,
   );
